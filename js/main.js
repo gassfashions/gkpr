a65019698c18cddd424495cf24f1863bf1969232
mark <mark@bluestar-apps.com>
2018-04-17 15:35:41 +0530

    work

--- js/main.js
+++ js/main.js
@@ -136,7 +136,7 @@ gateKeeperApp.controller('mainCtrl', function ($scope, $route, $routeParams, $lo
 });
 
 
-gateKeeperApp.controller('loginPageCtrl', function ($scope, $http, $cookies, $rootScope) {
+gateKeeperApp.controller('loginPageCtrl', function ($scope, $http, $cookies, $rootScope, CustomizeBase64) {
  
     $scope.$parent.contentFound = 0;   
     $scope.contentFound = 0;
@@ -164,7 +164,10 @@ gateKeeperApp.controller('loginPageCtrl', function ($scope, $http, $cookies, $ro
                 $('#loading_span_id').html('Validating');
                 $scope.$parent.contentFound = 0;   
                 $scope.contentFound = 0;
-
+                
+                $http.defaults.headers.common = {"Access-Control-Request-Headers": "accept, origin, authorization"}; //you probably don't need this line.  This lets me connect to my server on a different domain
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('saud' + ':' + 'Hello@123');
+    
                 $http.get(serviceBase + 'getValidateUser?flat_no='+encodeURIComponent(userObj.flat_no)+'&password='+encodeURIComponent(userObj.user_password)).then(function (data){
                     $scope.loginObj = data.data.data;
                     $scope.loginObjmsg = data.data.msg;
@@ -303,4 +306,90 @@ gateKeeperApp.controller('dashboardPageCtrl', function ($scope, $http, $rootScop
     $scope.contentFound = 1;
     
 
+});
+
+
+gateKeeperApp.factory('CustomizeBase64', function() {
+    var keyStr = 'ABCDEFGHIJKLMNOP' +
+            'QRSTUVWXYZabcdef' +
+            'ghijklmnopqrstuv' +
+            'wxyz0123456789+/' +
+            '=';
+    return {
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                alert("There were invalid base64 characters in the input text.\n" +
+                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                        "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
 });
